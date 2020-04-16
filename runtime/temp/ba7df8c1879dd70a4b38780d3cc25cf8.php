1983796a5d458d4c2e5633ae4b6c5e25c41d59ff
wuchuheng <root@wuchuheng.com>
2020-04-16 15:50:31 +0800

    示例

---
+++ runtime/temp/ba7df8c1879dd70a4b38780d3cc25cf8.php
@@ -0,0 +1,4 @@
+<?php /*a:1:{s:52:"/www/wwwroot/tp6.wuchuheng.com/view/index/index.html";i:1587023264;}*/ ?>
+字符串  <?php echo htmlentities($str); ?> </br>
+数组 <?php echo htmlentities($arr['a']); ?>  <br>
+对象 <?php echo htmlentities($obj->a); ?>
